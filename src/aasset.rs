7ef369794eb25e048650f9ca01e9a0162125a1ca
Sukrisus <ritaskritask5@gmail.com>
2025-08-02 21:03:45 +0530

    cape physics fix(again)

--- src/aasset.rs
+++ src/aasset.rs
@@ -33,10 +33,6 @@ const RENDER_CHUNK_NV_MATERIAL_BIN: &[u8] = include_bytes!("nightvision_material
 
 const CUSTOM_SPLASHES_JSON: &str = r#"{"splashes":["Origin Client","Origin > any other client","The Best Client!!","BlueCat","Origin is so much better","Origin Optimizes like no other client","Make Sure to star our repository:https://github.com/Origin-Client/Origin","Contributions open!","Made by the community, for the community","Yami is goated!!"]}"#;
 
-const CUSTOM_CAPE_GEOMETRY_JSON: &str = r#"{"texturewidth":64,"textureheight":32,"visible_bounds_width":2,"visible_bounds_height":3.5,"visible_bounds_offset":[0,1.25,0],"bones":[{"name":"root","pivot":[0,0,0]},{"name":"waist","parent":"root","pivot":[0,12,0]},{"name":"body","parent":"waist","pivot":[0,24,0]},{"name":"cape","parent":"body","pivot":[0,24,2],"rotation":[0,180,0]},{"name":"part1","parent":"cape","pivot":[0,24,2],"cubes":[{"origin":[-5,23,1],"size":[10,1,1],"uv":{"north":{"uv":[1,1],"uv_size":[10,1]},"east":{"uv":[0,1],"uv_size":[1,1]},"south":{"uv":[12,1],"uv_size":[10,1]},"west":{"uv":[11,1],"uv_size":[1,1]},"up":{"uv":[1,1],"uv_size":[10,-1]}}}]},{"name":"part2","parent":"part1","pivot":[0,23,1],"cubes":[{"origin":[-5,22,1],"size":[10,1.5,1],"uv":{"north":{"uv":[1,1.5],"uv_size":[10,1.5]},"east":{"uv":[0,1.5],"uv_size":[1,1.5]},"south":{"uv":[12,1.5],"uv_size":[10,1.5]},"west":{"uv":[11,1.5],"uv_size":[1,1.5]}}}]},{"name":"part3","parent":"part2","pivot":[0,22,1],"cubes":[{"origin":[-5,21,1],"size":[10,1.5,1],"uv":{"north":{"uv":[1,2.5],"uv_size":[10,1.5]},"east":{"uv":[0,2.5],"uv_size":[1,1.5]},"south":{"uv":[12,2.5],"uv_size":[10,1.5]},"west":{"uv":[11,2.5],"uv_size":[1,1.5]}}}]},{"name":"part4","parent":"part3","pivot":[0,21,1],"cubes":[{"origin":[-5,20,1],"size":[10,1.5,1],"uv":{"north":{"uv":[1,3.5],"uv_size":[10,1.5]},"east":{"uv":[0,3.5],"uv_size":[1,1.5]},"south":{"uv":[12,3.5],"uv_size":[10,1.5]},"west":{"uv":[11,3.5],"uv_size":[1,1.5]}}}]},{"name":"part5","parent":"part4","pivot":[0,20,1],"cubes":[{"origin":[-5,19,1],"size":[10,1.5,1],"uv":{"north":{"uv":[1,4.5],"uv_size":[10,1.5]},"east":{"uv":[0,4.5],"uv_size":[1,1.5]},"south":{"uv":[12,4.5],"uv_size":[10,1.5]},"west":{"uv":[11,4.5],"uv_size":[1,1.5]}}}]},{"name":"part6","parent":"part5","pivot":[0,19,1],"cubes":[{"origin":[-5,18,1],"size":[10,1.5,1],"uv":{"north":{"uv":[1,5.5],"uv_size":[10,1.5]},"east":{"uv":[0,5.5],"uv_size":[1,1.5]},"south":{"uv":[12,5.5],"uv_size":[10,1.5]},"west":{"uv":[11,5.5],"uv_size":[1,1.5]}}}]},{"name":"part7","parent":"part6","pivot":[0,18,1],"cubes":[{"origin":[-5,17,1],"size":[10,1.5,1],"uv":{"north":{"uv":[1,6.5],"uv_size":[10,1.5]},"east":{"uv":[0,6.5],"uv_size":[1,1.5]},"south":{"uv":[12,6.5],"uv_size":[10,1.5]},"west":{"uv":[11,6.5],"uv_size":[1,1.5]}}}]},{"name":"part8","parent":"part7","pivot":[0,17,1],"cubes":[{"origin":[-5,16,1],"size":[10,1.5,1],"uv":{"north":{"uv":[1,7.5],"uv_size":[10,1.5]},"east":{"uv":[0,7.5],"uv_size":[1,1.5]},"south":{"uv":[12,7.5],"uv_size":[10,1.5]},"west":{"uv":[11,7.5],"uv_size":[1,1.5]}}}]},{"name":"part9","parent":"part8","pivot":[0,16,1],"cubes":[{"origin":[-5,15,1],"size":[10,1.5,1],"uv":{"north":{"uv":[1,8.5],"uv_size":[10,1.5]},"east":{"uv":[0,8.5],"uv_size":[1,1.5]},"south":{"uv":[12,8.5],"uv_size":[10,1.5]},"west":{"uv":[11,8.5],"uv_size":[1,1.5]}}}]},{"name":"part10","parent":"part9","pivot":[0,15,1],"cubes":[{"origin":[-5,14,1],"size":[10,1.5,1],"uv":{"north":{"uv":[1,9.5],"uv_size":[10,1.5]},"east":{"uv":[0,9.5],"uv_size":[1,1.5]},"south":{"uv":[12,9.5],"uv_size":[10,1.5]},"west":{"uv":[11,9.5],"uv_size":[1,1.5]}}}]},{"name":"part11","parent":"part10","pivot":[0,14,1],"cubes":[{"origin":[-5,13,1],"size":[10,1.5,1],"uv":{"north":{"uv":[1,10.5],"uv_size":[10,1.5]},"east":{"uv":[0,10.5],"uv_size":[1,1.5]},"south":{"uv":[12,10.5],"uv_size":[10,1.5]},"west":{"uv":[11,10.5],"uv_size":[1,1.5]}}}]},{"name":"part12","parent":"part11","pivot":[0,13,1],"cubes":[{"origin":[-5,12,1],"size":[10,1.5,1],"uv":{"north":{"uv":[1,11.5],"uv_size":[10,1.5]},"east":{"uv":[0,11.5],"uv_size":[1,1.5]},"south":{"uv":[12,11.5],"uv_size":[10,1.5]},"west":{"uv":[11,11.5],"uv_size":[1,1.5]}}}]},{"name":"part13","parent":"part12","pivot":[0,12,1],"cubes":[{"origin":[-5,11,1],"size":[10,1.5,1],"uv":{"north":{"uv":[1,12.5],"uv_size":[10,1.5]},"east":{"uv":[0,12.5],"uv_size":[1,1.5]},"south":{"uv":[12,12.5],"uv_size":[10,1.5]},"west":{"uv":[11,12.5],"uv_size":[1,1.5]}}}]},{"name":"part14","parent":"part13","pivot":[0,11,1],"cubes":[{"origin":[-5,10,1],"size":[10,1.5,1],"uv":{"north":{"uv":[1,13.5],"uv_size":[10,1.5]},"east":{"uv":[0,13.5],"uv_size":[1,1.5]},"south":{"uv":[12,13.5],"uv_size":[10,1.5]},"west":{"uv":[11,13.5],"uv_size":[1,1.5]}}}]},{"name":"part15","parent":"part14","pivot":[0,10,1],"cubes":[{"origin":[-5,9,1],"size":[10,1.5,1],"uv":{"north":{"uv":[1,14.5],"uv_size":[10,1.5]},"east":{"uv":[0,14.5],"uv_size":[1,1.5]},"south":{"uv":[12,14.5],"uv_size":[10,1.5]},"west":{"uv":[11,14.5],"uv_size":[1,1.5]}}}]},{"name":"part16","parent":"part15","pivot":[0,9,1],"cubes":[{"origin":[-5,8,1],"size":[10,1.5,1],"uv":{"north":{"uv":[1,15.5],"uv_size":[10,1.5]},"east":{"uv":[0,15.5],"uv_size":[1,1.5]},"south":{"uv":[12,15.5],"uv_size":[10,1.5]},"west":{"uv":[11,15.5],"uv_size":[1,1.5]},"down":{"uv":[11,1],"uv_size":[10,-1]}}}]}]}"#;
-
-const CUSTOM_CAPE_ANIMATION_JSON: &str = r#"{"loop":true,"bones":{"cape":{"rotation":["math.clamp(math.lerp(0, -110, query.cape_flap_amount) - (13 * query.modified_move_speed), -70, 0)","query.modified_move_speed * math.pow(math.sin(query.body_y_rotation - query.head_y_rotation(0)), 3) * 55",0],"position":[0,0,"query.get_root_locator_offset('armor_offset.default_neck', 1)"]},"part1":{"rotation":["math.clamp(query.cape_flap_amount, 0, 0.5) * (math.cos(query.modified_distance_moved * 18) * 16)",0,"0"]},"part2":{"rotation":["math.clamp(query.cape_flap_amount, 0, 0.5) * math.cos(22 - query.modified_distance_moved * 18) * 13",0,0],"scale":1},"part3":{"rotation":["math.clamp(query.cape_flap_amount, 0, 0.5) * math.cos(50 - query.modified_distance_moved * 18) * 13",0,0]},"part4":{"rotation":["math.clamp(query.cape_flap_amount, 0, 0.5) * math.cos(76 - query.modified_distance_moved * 18) * 13",0,0]},"part5":{"rotation":["math.clamp(query.cape_flap_amount, 0, 0.5) * math.cos(100 - query.modified_distance_moved * 18) * 13",0,0]},"part6":{"rotation":["math.clamp(query.cape_flap_amount, 0, 0.5) * math.cos(122 - query.modified_distance_moved * 18) * 13",0,0]},"part7":{"rotation":["math.clamp(query.cape_flap_amount, 0, 0.5) * math.cos(142 - query.modified_distance_moved * 18) * 13",0,0]},"part8":{"rotation":["math.clamp(query.cape_flap_amount, 0, 0.5) * math.cos(160 - query.modified_distance_moved * 18) * 13",0,0]},"part9":{"rotation":["math.clamp(query.cape_flap_amount, 0, 0.5) * math.cos(176 - query.modified_distance_moved * 18) * 13",0,0]},"part10":{"rotation":["math.clamp(query.cape_flap_amount, 0, 0.5) * math.cos(190 - query.modified_distance_moved * 18) * 13",0,0]},"part11":{"rotation":["math.clamp(query.cape_flap_amount, 0, 0.5) * math.cos(202 - query.modified_distance_moved * 18) * 13",0,0]},"part12":{"rotation":["math.clamp(query.cape_flap_amount, 0, 0.5) * math.cos(212 - query.modified_distance_moved * 18) * 13",0,0]},"part13":{"rotation":["math.clamp(query.cape_flap_amount, 0, 0.5) * math.cos(220 - query.modified_distance_moved * 18) * 13",0,0]},"part14":{"rotation":["math.clamp(query.cape_flap_amount, 0, 0.5) * math.cos(226 - query.modified_distance_moved * 18) * 13",0,0]},"part15":{"rotation":["math.clamp(query.cape_flap_amount, 0, 0.5) * math.cos(230 - query.modified_distance_moved * 18) * 13",0,0]},"part16":{"rotation":["math.clamp(query.cape_flap_amount, 0, 0.5) * math.cos(232 - query.modified_distance_moved * 18) * 13",0,0]},"shoulders":{"rotation":[0,"query.modified_move_speed * math.pow(math.sin(query.body_y_rotation - query.head_y_rotation(0)), 3) * 60",0]}}}"#;
-
 const CUSTOM_FIRST_PERSON_JSON: &str = r#"{"format_version":"1.18.10","minecraft:camera_entity":{"description":{"identifier":"minecraft:first_person"},"components":{"minecraft:camera":{"field_of_view":66,"near_clipping_plane":0.025,"far_clipping_plane":2500},"minecraft:camera_first_person":{},"minecraft:camera_render_first_person_objects":{},"minecraft:camera_attach_to_player":{},"minecraft:camera_offset":{"view":[0,0],"entity":[0,0,0]},"minecraft:camera_direct_look":{"pitch_min":-89.9,"pitch_max":89.9},"minecraft:camera_perspective_option":{"view_mode":"first_person"},"minecraft:update_player_from_camera":{"look_mode":"along_camera"},"minecraft:extend_player_rendering":{},"minecraft:camera_player_sleep_vignette":{},"minecraft:vr_comfort_move":{},"minecraft:default_input_camera":{},"minecraft:gameplay_affects_fov":{},"minecraft:allow_inside_block":{}}}}"#;
 const CUSTOM_THIRD_PERSON_JSON: &str = r#"{"format_version":"1.18.10","minecraft:camera_entity":{"description":{"identifier":"minecraft:third_person"},"components":{"minecraft:camera":{"field_of_view":66,"near_clipping_plane":0.025,"far_clipping_plane":2500},"minecraft:camera_third_person":{},"minecraft:camera_render_player_model":{},"minecraft:camera_attach_to_player":{},"minecraft:camera_offset":{"view":[0,0],"entity":[0,2,5]},"minecraft:camera_look_at_player":{},"minecraft:camera_orbit":{"azimuth_smoothing_spring":0,"polar_angle_smoothing_spring":0,"distance_smoothing_spring":0,"polar_angle_min":0.1,"polar_angle_max":179.9,"radius":4},"minecraft:camera_avoidance":{"relax_distance_smoothing_spring":0,"distance_constraint_min":0.25},"minecraft:camera_perspective_option":{"view_mode":"third_person"},"minecraft:update_player_from_camera":{"look_mode":"along_camera"},"minecraft:camera_player_sleep_vignette":{},"minecraft:gameplay_affects_fov":{},"minecraft:allow_inside_block":{},"minecraft:extend_player_rendering":{}}}}"#;
 const CUSTOM_THIRD_PERSON_FRONT_JSON: &str = r#"{"format_version":"1.18.10","minecraft:camera_entity":{"description":{"identifier":"minecraft:third_person_front"},"components":{"minecraft:camera":{"field_of_view":66,"near_clipping_plane":0.025,"far_clipping_plane":2500},"minecraft:camera_third_person":{},"minecraft:camera_render_player_model":{},"minecraft:camera_attach_to_player":{},"minecraft:camera_offset":{"view":[0,0],"entity":[0,2,5]},"minecraft:camera_look_at_player":{},"minecraft:camera_orbit":{"azimuth_smoothing_spring":0,"polar_angle_smoothing_spring":0,"distance_smoothing_spring":0,"polar_angle_min":0.1,"polar_angle_max":179.9,"radius":4,"invert_x_input":true},"minecraft:camera_avoidance":{"relax_distance_smoothing_spring":0,"distance_constraint_min":0.25},"minecraft:camera_perspective_option":{"view_mode":"third_person_front"},"minecraft:update_player_from_camera":{"look_mode":"at_camera"},"minecraft:camera_player_sleep_vignette":{},"minecraft:gameplay_affects_fov":{},"minecraft:allow_inside_block":{},"minecraft:extend_player_rendering":{}}}}"#;
@@ -247,94 +243,26 @@ fn is_persona_file_to_block(c_path: &Path) -> bool {
     })
 }
 
-// Cape physics helper functions - Modified to patch existing files
-fn is_mobs_json_file(c_path: &Path) -> bool {
+fn get_cape_model_data(filename: &str) -> Option<&'static [u8]> {
     if !is_cape_physics_enabled() {
-        return false;
+        return None;
     }
     
-    let path_str = c_path.to_string_lossy();
-    
-    // Check for mobs.json in models folder
-    let mobs_patterns = [
-        "models/mobs.json",
-        "/models/mobs.json",
-        "vanilla/models/mobs.json",
-        "resource_packs/vanilla/models/mobs.json",
-        "assets/resource_packs/vanilla/models/mobs.json",
-    ];
-    
-    mobs_patterns.iter().any(|pattern| {
-        path_str.contains(pattern) || path_str.ends_with(pattern)
-    })
+    match filename {
+        "mobs.json" => Some(MOBS_JSON),
+        _ => None,
+    }
 }
 
-fn is_player_animation_file(c_path: &Path) -> bool {
+fn get_cape_animation_data(filename: &str) -> Option<&'static [u8]> {
     if !is_cape_physics_enabled() {
-        return false;
+        return None;
     }
     
-    let path_str = c_path.to_string_lossy();
-    
-    // Check for player.animation.json in animations folder
-    let animation_patterns = [
-        "animations/player.animation.json",
-        "/animations/player.animation.json",
-        "vanilla/animations/player.animation.json",
-        "resource_packs/vanilla/animations/player.animation.json",
-        "assets/resource_packs/vanilla/animations/player.animation.json",
-    ];
-    
-    animation_patterns.iter().any(|pattern| {
-        path_str.contains(pattern) || path_str.ends_with(pattern)
-    })
-}
-
-fn patch_mobs_json(original_data: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
-    // Parse the original mobs.json
-    let mut json_value: Value = serde_json::from_slice(original_data)?;
-    
-    // Parse our custom cape geometry (as a complete geometry object)
-    let cape_geometry: Value = serde_json::from_str(CUSTOM_CAPE_GEOMETRY_JSON)?;
-    
-    // The mobs.json file uses direct geometry names as keys, not an array structure
-    // We need to replace the "geometry.cape" key directly
-    if let Some(obj) = json_value.as_object_mut() {
-        // Replace the existing geometry.cape with our custom one
-        obj.insert("geometry.cape".to_string(), cape_geometry);
-        log::info!("Successfully replaced geometry.cape in mobs.json");
-    } else {
-        return Err("mobs.json root is not an object".into());
+    match filename {
+        "player.animation.json" => Some(PLAYER_ANIMATION_JSON),
+        _ => None,
     }
-    
-    // Serialize back to JSON with pretty formatting
-    let patched_data = serde_json::to_vec_pretty(&json_value)?;
-    Ok(patched_data)
-}
-
-fn patch_player_animation_json(original_data: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
-    // Parse the original player.animation.json
-    let mut json_value: Value = serde_json::from_slice(original_data)?;
-    
-    // Parse our custom cape animation (just the animation object)
-    let cape_animation: Value = serde_json::from_str(CUSTOM_CAPE_ANIMATION_JSON)?;
-    
-    // Ensure animations object exists
-    if json_value["animations"].is_null() {
-        json_value["animations"] = serde_json::json!({});
-    }
-    
-    // Replace the existing animation.player.cape
-    if let Some(animations) = json_value["animations"].as_object_mut() {
-        animations.insert("animation.player.cape".to_string(), cape_animation);
-        log::info!("Successfully replaced animation.player.cape in player.animation.json");
-    } else {
-        return Err("animations section is not an object".into());
-    }
-    
-    // Serialize back to JSON with pretty formatting
-    let patched_data = serde_json::to_vec_pretty(&json_value)?;
-    Ok(patched_data)
 }
 
 pub(crate) unsafe fn open(
@@ -354,12 +282,6 @@ pub(crate) unsafe fn open(
     };
 
     // Debug logging for features
-    if is_cape_physics_enabled() {
-        let path_str = c_path.to_string_lossy();
-        if path_str.contains("cape") || path_str.contains("animation") || path_str.contains("entity") || path_str.contains("mobs.json") || path_str.contains("player.animation.json") {
-            log::info!("Cape physics enabled - checking file: {}", c_path.display());
-        }
-    }
     
     if is_particles_disabler_enabled() {
         let path_str = c_path.to_string_lossy();
@@ -404,65 +326,6 @@ pub(crate) unsafe fn open(
         return aasset;
     }
     
-    // Cape physics patching - Patch existing files instead of creating new ones
-    if is_mobs_json_file(c_path) && !aasset.is_null() {
-        log::info!("Cape physics: Patching mobs.json with custom cape geometry: {}", c_path.display());
-        
-        // Read the original file
-        let original_length = ndk_sys::AAsset_getLength(aasset) as usize;
-        let mut original_data = vec![0u8; original_length];
-        let read_result = ndk_sys::AAsset_read(aasset, original_data.as_mut_ptr() as *mut libc::c_void, original_length);
-        
-        if read_result == original_length as i32 {
-            match patch_mobs_json(&original_data) {
-                Ok(patched_data) => {
-                    let mut wanted_lock = WANTED_ASSETS.lock().unwrap();
-                    wanted_lock.insert(AAssetPtr(aasset), Cursor::new(patched_data));
-                    return aasset;
-                }
-                Err(e) => {
-                    log::error!("Failed to patch mobs.json: {}", e);
-                    // Return original asset on error
-                }
-            }
-        } else {
-            log::error!("Failed to read original mobs.json data");
-        }
-        
-        // Reset asset position on error
-        ndk_sys::AAsset_seek(aasset, 0, libc::SEEK_SET);
-        return aasset;
-    }
-    
-    if is_player_animation_file(c_path) && !aasset.is_null() {
-        log::info!("Cape physics: Patching player.animation.json with custom cape animation: {}", c_path.display());
-        
-        // Read the original file
-        let original_length = ndk_sys::AAsset_getLength(aasset) as usize;
-        let mut original_data = vec![0u8; original_length];
-        let read_result = ndk_sys::AAsset_read(aasset, original_data.as_mut_ptr() as *mut libc::c_void, original_length);
-        
-        if read_result == original_length as i32 {
-            match patch_player_animation_json(&original_data) {
-                Ok(patched_data) => {
-                    let mut wanted_lock = WANTED_ASSETS.lock().unwrap();
-                    wanted_lock.insert(AAssetPtr(aasset), Cursor::new(patched_data));
-                    return aasset;
-                }
-                Err(e) => {
-                    log::error!("Failed to patch player.animation.json: {}", e);
-                    // Return original asset on error
-                }
-            }
-        } else {
-            log::error!("Failed to read original player.animation.json data");
-        }
-        
-        // Reset asset position on error
-        ndk_sys::AAsset_seek(aasset, 0, libc::SEEK_SET);
-        return aasset;
-    }
-
     // Java clouds texture replacement
     if is_clouds_texture_file(c_path) {
         log::info!("Intercepting clouds texture with Java clouds texture: {}", c_path.display());
@@ -541,6 +404,22 @@ pub(crate) unsafe fn open(
     if let Some(night_vision_data) = get_nightvision_material_data(&filename_str) {
         log::info!("Intercepting {} with night-vision material (night-vision enabled)", filename_str);
         let buffer = night_vision_data.to_vec();
+        let mut wanted_lock = WANTED_ASSETS.lock().unwrap();
+        wanted_lock.insert(AAssetPtr(aasset), Cursor::new(buffer));
+        return aasset;
+    }
+    
+    if let Some(cape_physics_animation_data) = get_cape_animation_data(&filename_str) {
+        log::info!("Intercepting {} with cape-physics animation (cape-physics enabled)", filename_str);
+        let buffer = cape_physics_animation_data.to_vec();
+        let mut wanted_lock = WANTED_ASSETS.lock().unwrap();
+        wanted_lock.insert(AAssetPtr(aasset), Cursor::new(buffer));
+        return aasset;
+    }
+    
+    if let Some(cape_physics_model_data) = get_cape_model_data(&filename_str) {
+        log::info!("Intercepting {} with cape-physics model (cape-physics enabled)", filename_str);
+        let buffer = cape_physics_model_data.to_vec();
         let mut wanted_lock = WANTED_ASSETS.lock().unwrap();
         wanted_lock.insert(AAssetPtr(aasset), Cursor::new(buffer));
         return aasset;
