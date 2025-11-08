b40d0c38ed0ba2ec2fe1e14ff0ceacd75a9eed65
VCX <88580298+Viablecobra@users.noreply.github.com>
2025-11-08 22:12:16 +0530

    Add files via upload

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
 fn main() {
+    println!("cargo:rustc-link-search=native={}", "preloader");
+    println!("cargo:rustc-link-lib=preloader");
     cc::Build::new()
         .cpp(true)
         .file("src/string.cpp")
