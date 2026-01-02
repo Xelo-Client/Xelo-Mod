ec58c74780a6b0be4979357a4895f39bb223a543
vcx <ryansk0987@gmail.com>
2026-01-02 14:14:15 +0530

    ...

--- build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    println!("cargo:rustc-link-search=native={}", "preloader");
-    println!("cargo:rustc-link-lib=preloader");
-    cc::Build::new()
-        .cpp(true)
-        .file("src/string.cpp")
-        .compile("stringstub");
-}
